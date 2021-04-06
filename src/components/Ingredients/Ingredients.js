1cf0be0ae80dea370f8cfa0052eda19fdc433368
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-04-05 18:45:47 -0600

    fetching (post and delete) from firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from './IngredientList';
@@ -6,20 +6,64 @@ import Search from "./Search";
 
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
+/*
+    useEffect(() => {
+        fetch("https://ingredient-card-hooks-default-rtdb.firebaseio.com/ingredients.json")
+            .then(response => response.json())
+            .then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+                loadedIngredients.push({
+                    id: key,
+                    title: responseData[key].title,
+                    amount: responseData[key].amount
+                });
+            }
+            setUserIngredients(loadedIngredients);
+          });
+    }, []);
+*/
+    const filteredIngredientsHandler = useCallback(filteredIngredients => {
+        setUserIngredients(filteredIngredients);
+      }, []);
 
     const addIngredientHandler = (ingredient) => {
-        setUserIngredients(prevIngredients => [
-            ...prevIngredients,
-            { id: Math.random().toString(), ...ingredient }
-        ]);
+        fetch(
+            "https://ingredient-card-hooks-default-rtdb.firebaseio.com/ingredients.json",
+            {
+                method: "POST",
+                body: JSON.stringify(ingredient),
+                headers: { "Content-Type": "application/json" },
+            })
+            .then(response => {
+                return response.json;
+            })
+            .then(responseData => {
+                setUserIngredients(prevIngredients => [
+                    ...prevIngredients,
+                    { id: responseData.name, ...ingredient }
+                ]);
+            });
     };
+
+    const removeIngredientHandler = ingredientId => {
+        fetch(`https://ingredient-card-hooks-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, 
+        {
+            method: 'DELETE'
+        })
+        .then(response => {
+            setUserIngredients(prevIngredients =>
+                prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+            );
+        })
+      };
 
     return (
         <div className="App">
             <IngredientForm onAddIngredient={addIngredientHandler} />
             <section>
-                <Search />
-                <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+                <Search onLoadIngredients={filteredIngredientsHandler}/>
+                <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
             </section>
         </div>
     );
